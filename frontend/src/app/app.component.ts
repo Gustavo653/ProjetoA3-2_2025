2b363e4d9baf2fa57940a747cf026e73ce09b664
Gustavo <gustavohs2004@gmail.com>
2025-05-19 21:12:03 -0300

    Correções para tela abrir

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor(private auth: AuthService, private router: Router){}
+  constructor(private auth: AuthService, private router: Router) {}
   loggedIn = this.auth.loggedIn;
-  logout(){
+
+  logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
